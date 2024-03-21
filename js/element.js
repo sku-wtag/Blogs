3da9753d93c1cd37e5d381b8b891705d363bf4e0
Shorojit Kumar Sarkar <152980765+sku-wtag@users.noreply.github.com>
2024-03-21 08:54:14 +0600

    add search features

--- js/element.js
+++ js/element.js
@@ -4,4 +4,6 @@ export const addButton = document.getElementById("add_button")
 export const formContainer = document.getElementById('show_form');
 export const form = document.createElement('form');
 export const editForm = document.createElement('form');
+export const searchBox = document.getElementById("search_box")
+
 export const baseURL = "https://jsonplaceholder.typicode.com"
